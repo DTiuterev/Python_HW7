e2e8e80157b6426113ce4cab5c3d2aef39530221
Dmitrii <dtiuterev@rambler.ru>
2022-11-26 21:44:29 +0300

    Added logger.py

---
+++ logger.py
@@ -0,0 +1,11 @@
+from datetime import datetime as dt
+
+def log_info(data):
+    time = dt.now().strftime('%D %H:%M')
+    with open('log.txt', 'a') as file:
+        file.write(f'{time} info: {data}\n')
+
+def log_error(data):
+    time = dt.now().strftime('%D %H:%M')
+    with open('log.txt', 'a') as file:
+        file.write(f'{time} error: {data}\n')
